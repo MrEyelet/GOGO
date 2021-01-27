7dea1fdabd3125579d25668d32c9f9e31c572ded
Marcin <marcin.adam.mroczko@gmail.com>
2021-01-28 00:16:24 +0100

    slider

---
+++ js/modules/Slider.js
@@ -0,0 +1,34 @@
+import Swiper from "swiper/bundle"
+import "swiper/swiper-bundle.css"
+
+class Slider {
+  constructor() {
+    this.runSlider()
+  }
+
+  runSlider() {
+    const mySwiper = new Swiper(".swiper-container", {
+      direction: "horizontal",
+      loop: true,
+      // slidesPerView: 3,
+      spaceBetween: 15,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1
+        },
+        480: {
+          slidesPerView: 2
+        },
+        768: {
+          slidesPerView: 3
+        }
+      }
+    })
+  }
+}
+
+export default Slider
